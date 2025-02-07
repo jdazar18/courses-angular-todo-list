54ab427e2e54f56d25d96fcda64c7ad9596bd783
Julian Daza <jdazar@unal.edu.co>
2025-02-07 16:20:40 -0500

    feat: added add todo functionality

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-todo-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.css'
 })
 export class TodoListComponent {
+
+  todos : string[];
+
+  constructor() {
+    this.todos = [];
+  }
+
+  addTodo(todo : string) {
+    this.todos.push(todo);
+    return false;
+  }
 
 }
